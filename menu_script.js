d86fc7d20886d1b34c0bfcd03bc0598e8f2d04b0
Chukwuemelie Uyammadu <138344309+Bobbygkmc@users.noreply.github.com>
2023-12-27 15:24:25 -0500

    Create menu_script.js

---
+++ menu_script.js
@@ -0,0 +1,9 @@
+// Toggle navigation menu
+function toggleMenu() {
+    var menu = document.getElementById("navMenu");
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+    } else {
+        menu.classList.add("active");
+    }
+}
